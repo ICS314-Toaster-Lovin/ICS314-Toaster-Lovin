6054a81e635b3923e7cd06a338b836e1bf6a1960
Bradley <60422096+b-valiente@users.noreply.github.com>
2022-11-15 00:36:17 -1000

    Finished Landing Page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,14 +1,52 @@
 import React from 'react';
-import { Container, Image } from 'react-bootstrap';
+import { Button, Carousel } from 'react-bootstrap';
 
 /* A simple static component to render some text for the landing page. */
 const Landing = () => (
 
-  <Container className="container-fluid">
+  <Carousel variant="dark">
+    <Carousel.Item>
+      <img
+        id="img"
+        className="d-block w-100"
+        src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg"
+        alt="First slide"
+      />
 
-    <Image className="img-fluid" src="https://media.self.com/photos/622912847b959736301bfb91/4:3/w_2240,c_limit/GettyImages-1301412050.jpg" />
+      <Carousel.Caption>
+        <Button className="justify-content-center" href="http://localhost:3000/signup">Sign Up Today</Button>
+        <p>The goal of this site is to help improve the nutritional content and variety of foods eaten by students and help them limit the use of vending machines or fast food products by providing tasty, realistic alternatives.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        id="img"
+        className="d-block w-100"
+        src="https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="Second slide"
+      />
 
-  </Container>
+      <Carousel.Caption>
+        <Button className="justify-content-center" href="http://localhost:3000/signup">Sign Up Today</Button>
+        <p>The goal of this site is to help improve the nutritional content and variety of foods eaten by students and help them limit the use of vending machines or fast food products by providing tasty, realistic alternatives.</p>
+      </Carousel.Caption>
+    </Carousel.Item>
+    <Carousel.Item>
+      <img
+        id="img"
+        className="d-block w-100"
+        src="https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="Third slide"
+      />
+
+      <Carousel.Caption>
+        <Button className="justify-content-center" href="http://localhost:3000/signup">Sign Up Today</Button>
+        <p>
+          The goal of this site is to help improve the nutritional content and variety of foods eaten by students and help them limit the use of vending machines or fast food products by providing tasty, realistic alternatives.
+        </p>
+      </Carousel.Caption>
+    </Carousel.Item>
+  </Carousel>
 );
 
 export default Landing;
