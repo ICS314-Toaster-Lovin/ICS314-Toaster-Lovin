50240a76847407a2d16e68dc1f8b622d2047103c
khirano7 <khirano7@hawaii.edu>
2022-11-16 20:53:17 -1000

    Updated Footer

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import { Col, Container } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 bg-light">
     <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        {' '}
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        {' '}
-        <br />
-        <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
-          Template Home
-          Page
-        </a>
-      </Col>
+      <Row className="pb-3">
+        <Col className="text-center">
+          <br />
+          <br />
+          <a href="https://no-more-ramen.github.io/">
+            Github Repository
+          </a>
+        </Col>
+        <Col className="text-center">
+          Resources:
+          <br />
+          <br />
+          <a href="https://no-more-ramen.github.io/">
+            Visit our Github Page
+          </a>
+        </Col>
+        <Col className="text-center">
+          <br />
+          <br />
+          <a href="http://ics-software-engineering.github.io/meteor-application-template-react">
+            Template Home Page
+          </a>
+        </Col>
+      </Row>
     </Container>
   </footer>
 );
