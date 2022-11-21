cb328d2a56ea8225c25fa0590af2e3290bd151f5
shannonkam <skam7@hawaii.edu>
2022-11-21 12:35:23 -1000

    Added Student Collection

---
+++ app/imports/api/student/Student.js
@@ -0,0 +1,32 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The RecipeCollection. It encapsulates state and variable values for stuff.
+ */
+class StudentCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'StudentCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+      dietary: String,
+      owner: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.student`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+/**
+ * The singleton instance of the StudentCollection.
+ * @type {StudentCollection}
+ */
+export const Student = new StudentCollection();
