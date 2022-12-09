d8eb640e09293f56ba86376ab833d2d6fe3d5e8b
1khanh <khanh2@hawaii.edu>
2022-12-08 19:43:35 -1000

    updated user list
    updates to userlist page UI

--- app/imports/ui/components/UserListItem.jsx
+++ app/imports/ui/components/UserListItem.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
-const UserListItem = ({ userlistitem }) => (
+const UserListItem = ({ userlistitem, deleteItem }) => (
   <tr>
     <td>{userlistitem.email}</td>
     <td>{userlistitem.role}</td>
     <td><Link to={`/edit-user-list/${userlistitem._id}`} id="edituserlist-link">Edit</Link></td>
+    <td><Button variant="danger" onClick={() => deleteItem(userlistitem._id)}>X</Button></td>
   </tr>
 );
 
@@ -18,6 +20,7 @@ UserListItem.propTypes = {
     role: PropTypes.string,
     _id: PropTypes.string,
   }).isRequired,
+  deleteItem: PropTypes.func.isRequired,
 };
 
 export default UserListItem;
